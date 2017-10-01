b89ffb4d605ffbf5f30e26c2737a321a6dc10d84
Vanessa Dean <vanessa.dean@gmail.com>
2017-10-01 13:46:33 -0400

    Production db setup

--- app.rb
+++ app.rb
@@ -8,6 +8,19 @@ configure :development do
   set :database, {adapter: "sqlite3", database: "cnycn.sqlite3"}
 end
 
+configure :production do
+ db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/database')
+
+ ActiveRecord::Base.establish_connection(
+   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+   :host     => db.host,
+   :username => db.user,
+   :password => db.password,
+   :database => db.path[1..-1],
+   :encoding => 'utf8'
+ )
+end
+
 get '/' do
   @clients = Client.all
   erb :index
