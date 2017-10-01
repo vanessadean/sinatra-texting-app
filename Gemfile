418a877d6d768c4c520fa8b948e92f010e56ef3e
Vanessa Dean <vanessa.dean@gmail.com>
2017-10-01 12:10:34 -0400

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+
+gem "sinatra"
+# gem "sinatra-activerecord"
+# gem "rake"
+# gem "tux"
+gem "dotenv"
+gem "thin"
+gem "twilio-ruby"
+
+group :development do
+  gem "sinatra-reloader"
+  gem "pry"
+  gem "sqlite3"
+end
+
+group :production do
+  gem "pg"
+end
---
+++ app.rb
@@ -0,0 +1,32 @@
+require 'dotenv'
+Dotenv.load
+
+require 'pry'
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'twilio-ruby'
+
+get '/' do
+  erb :index
+end
+
+post '/send' do
+  account_sid = ENV['TWILIO_SID'] # Your Account SID from www.twilio.com/console
+  auth_token = ENV['TWILIO_AUTH_TOKEN']   # Your Auth Token from www.twilio.com/console
+
+  begin
+    @client = Twilio::REST::Client.new account_sid, auth_token
+    message = @client.messages.create(
+        body: params['message'],
+        to: "+1#{params['phone_number']}",
+        from: '+13479411418')  # Replace with your Twilio number
+
+    puts message.sid
+  rescue Twilio::REST::TwilioError => e
+    puts e.message
+  end
+end
+
+post '/receive' do
+  binding.pry
+end
